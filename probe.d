34dd506e1a540afb66be07a4b60430f96c29dfdd
wesley lim <wesleylim.work@gmail.com>
2025-02-14 16:50:57 +0800

    feat: Update probe to filter only rocksdb function calls

--- probe.d
+++ probe.d
@@ -1,14 +1,14 @@
 #pragma D option quiet
 
 /* Trace function entry and exit for rocksdb::DB::Get and log probe details */
-pid$target:::entry
+pid$target:librocksdb_debug.9.11.0.dylib::entry
 {
     printf("Probe triggered: provider=%s, module=%s, function=%s, name=%s\n",
            probeprov, probemod, probefunc, probename);
     self->start = timestamp;
 }
 
-pid$target:::return
+pid$target:librocksdb_debug.9.11.0.dylib::return
 {
     printf("Elapsed time for function %s.%s.%s: %d ns\n", 
            probeprov, probemod, probefunc, timestamp - self->start);
