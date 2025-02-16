7669ac84d8f9ee2fefe599501b90d9c9a3e8742b
wesley lim <wesleylim.work@gmail.com>
2025-02-16 22:03:05 +0800

    feat: Update shell script to to be generic by type of DB operation

--- dtrace.sh
+++ dtrace.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+# Usage message
+usage() {
+  echo "Usage: sudo sh dtrace.sh -t <dtrace file path> -e <executable file path>"
+  exit 1
+}
+
 SETUP_DB_NAME="setup_db"
-POINT_LOOK_NAME="point_lookup"
+EXEC_NAME="point_lookup"
 DTRACE_SCRIPT="probe.d" # Default DTrace script
 
 # Parse arguments
-while getopts "p:" opt; do
+while getopts "t:e:" opt; do
   case ${opt} in
-    p )
+    t )
       DTRACE_SCRIPT=$OPTARG
       ;;
+    e )
+      EXEC_NAME=$OPTARG
+      ;;
     \? )
-      echo "Usage: $0 [-p dtrace_script]"
-      exit 1
+      usage
       ;;
   esac
 done
@@ -20,8 +28,8 @@ done
 echo "Compiling $SETUP_DB_NAME...\n"
 sudo g++ -std=c++17 -g -O0 -fno-inline profiling/common/setup.cpp -o $SETUP_DB_NAME -I./include -L./ -L/opt/homebrew/lib -lrocksdb -lz -lbz2 -lzstd -llz4 -pthread
 
-echo "Compiling $POINT_LOOK_NAME...\n"
-sudo g++ -std=c++17 -g -O0 -fno-inline profiling/point_lookup/get.cpp -o $POINT_LOOK_NAME -I./include -L./ -L/opt/homebrew/lib -lrocksdb -lz -lbz2 -lzstd -llz4 -pthread
+echo "Compiling $EXEC_NAME...\n"
+sudo g++ -std=c++17 -g -O0 -fno-inline profiling/point_lookup/get.cpp -o $EXEC_NAME -I./include -L./ -L/opt/homebrew/lib -lrocksdb -lz -lbz2 -lzstd -llz4 -pthread
 
 echo "Check db empty before writing:\n"
 ls /tmp/testdb
@@ -31,7 +39,11 @@ echo "Executed setup_db, ls /tmp/testdb:\n"
 ls /tmp/testdb
 
 echo "Running DTrace with script: $DTRACE_SCRIPT"
-sudo dtrace -s "$DTRACE_SCRIPT" -c ./$POINT_LOOK_NAME > probe_output.txt
+
+# Ensure profiling directory exists
+mkdir -p profiling/$EXEC_NAME
+
+sudo dtrace -s $DTRACE_SCRIPT -c ./$EXEC_NAME > "profiling/$EXEC_NAME/dtrace.txt"
 
 echo "Executed point_lookup\n"
 sudo rm -rf /tmp/testdb/*
